322349d001b9306a6b73898b76d813b3db50d09a
Miguelpr123 <miguelbyethost00@gmail.com>
2022-07-21 18:22:53 -0400

    Init

---
+++ js/logo.js
@@ -0,0 +1,320 @@
+
+
+window.addEventListener('load' , ()=>{
+
+    elemento = document.querySelector('#LogotipoPrincipal');
+    ctx = elemento.getContext('2d');
+
+    ctx.beginPath()
+    
+    // ctx.fillRect( 20 , 20 , 20 , 20);
+    ctx.moveTo(25,20)
+    ctx.lineTo(29,20)
+    ctx.lineTo(29,18)
+    ctx.lineTo(31,18)
+    ctx.lineTo(31,20)
+
+    ctx.lineTo(35,20)
+    ctx.lineTo(35,24)
+    ctx.lineTo(36,24)
+    ctx.lineTo(36,26)
+    ctx.lineTo(35,26)
+
+
+    ctx.lineTo(35,30)
+    ctx.lineTo(31,30)
+    ctx.lineTo(31,32)
+    ctx.lineTo(29,32)
+    ctx.lineTo(29,30)
+
+
+    ctx.lineTo(25,30)
+    ctx.lineTo(25,26)
+    ctx.lineTo(24,26)
+    ctx.lineTo(24,24)
+    ctx.lineTo(25,24)
+
+    ctx.fillStyle="#dc3545"
+    ctx.closePath()
+    ctx.fill()
+
+    
+    ctx.beginPath()
+    ctx.arc( 30 , 25 , 2 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = '#fff';
+    ctx.fill()
+    ctx.closePath()
+
+
+    ctx.beginPath()
+    ctx.moveTo( 28 , 8)
+    ctx.lineTo( 28 , 18)
+    ctx.lineTo( 23 , 18)
+    ctx.lineTo( 23 , 23 )
+    ctx.lineTo( 13 , 23 )
+    ctx.fillStyle = 'midnightblue';
+    ctx.strokeStyle = 'midnightblue';
+    ctx.closePath()
+    ctx.fill()
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.moveTo( 32 , 8)
+    ctx.lineTo( 32 , 18)
+    ctx.lineTo( 37 , 18)
+    ctx.lineTo( 37 , 23 )
+    ctx.lineTo( 47 , 23 )
+    ctx.fillStyle = 'midnightblue';
+    ctx.strokeStyle = 'midnightblue';
+    ctx.closePath()
+    ctx.fill()
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.moveTo( 32 , 42)
+    ctx.lineTo( 32 , 32)
+    ctx.lineTo( 37 , 32)
+    ctx.lineTo( 37 , 27 )
+    ctx.lineTo( 47 , 27 )
+    ctx.fillStyle = 'midnightblue';
+    ctx.strokeStyle = 'midnightblue';
+    ctx.closePath()
+    ctx.fill()
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.moveTo( 28 , 42)
+    ctx.lineTo( 28 , 32)
+    ctx.lineTo( 23 , 32)
+    ctx.lineTo( 23 , 27 )
+    ctx.lineTo( 13 , 27 )
+    ctx.fillStyle = 'midnightblue';
+    ctx.strokeStyle = 'midnightblue';
+    ctx.closePath()
+    ctx.fill()
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.arc( 23 , 39 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = '#fff';
+    ctx.fill()
+    ctx.closePath()
+
+    ctx.beginPath()
+    ctx.arc( 23 , 12 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = '#fff';
+    ctx.fill()
+    ctx.closePath()
+
+    ctx.beginPath()
+    ctx.arc( 37 , 39 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = '#fff';
+    ctx.fill()
+    ctx.closePath()
+
+    ctx.beginPath()
+    ctx.arc( 37 , 12 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = '#fff';
+    ctx.fill()
+    ctx.closePath()
+
+
+    ctx.beginPath()
+    ctx.arc( 45 , 32 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = '#fff';
+    ctx.fill()
+    ctx.closePath()
+
+    ctx.beginPath()
+    ctx.arc( 45 , 18 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = '#fff';
+    ctx.fill()
+    ctx.closePath()
+
+    ctx.beginPath()
+    ctx.arc( 15 , 18 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = '#fff';
+    ctx.fill()
+    ctx.closePath()
+
+    ctx.beginPath()
+    ctx.arc( 15 , 32 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = '#fff';
+    ctx.fill()
+    ctx.closePath()
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener('load' , ()=>{
+
+    elemento = document.querySelector('#LogotipoSecundario');
+    ctx = elemento.getContext('2d');
+
+    ctx.beginPath()
+    
+    // ctx.fillRect( 20 , 20 , 20 , 20);
+    ctx.moveTo(25,20)
+    ctx.lineTo(29,20)
+    ctx.lineTo(29,18)
+    ctx.lineTo(31,18)
+    ctx.lineTo(31,20)
+
+    ctx.lineTo(35,20)
+    ctx.lineTo(35,24)
+    ctx.lineTo(36,24)
+    ctx.lineTo(36,26)
+    ctx.lineTo(35,26)
+
+
+    ctx.lineTo(35,30)
+    ctx.lineTo(31,30)
+    ctx.lineTo(31,32)
+    ctx.lineTo(29,32)
+    ctx.lineTo(29,30)
+
+
+    ctx.lineTo(25,30)
+    ctx.lineTo(25,26)
+    ctx.lineTo(24,26)
+    ctx.lineTo(24,24)
+    ctx.lineTo(25,24)
+
+    ctx.fillStyle="#6c757d"
+    ctx.closePath()
+    ctx.fill()
+
+    
+    ctx.beginPath()
+    ctx.arc( 30 , 25 , 2 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = 'midnightblue';
+    ctx.fill()
+    ctx.closePath()
+
+
+    ctx.beginPath()
+    ctx.moveTo( 28 , 8)
+    ctx.lineTo( 28 , 18)
+    ctx.lineTo( 23 , 18)
+    ctx.lineTo( 23 , 23 )
+    ctx.lineTo( 13 , 23 )
+    ctx.fillStyle = '#6c757d';
+    ctx.strokeStyle = '#6c757d';
+    ctx.closePath()
+    ctx.fill()
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.moveTo( 32 , 8)
+    ctx.lineTo( 32 , 18)
+    ctx.lineTo( 37 , 18)
+    ctx.lineTo( 37 , 23 )
+    ctx.lineTo( 47 , 23 )
+    ctx.fillStyle = '#6c757d';
+    ctx.strokeStyle = '#6c757d';
+    ctx.closePath()
+    ctx.fill()
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.moveTo( 32 , 42)
+    ctx.lineTo( 32 , 32)
+    ctx.lineTo( 37 , 32)
+    ctx.lineTo( 37 , 27 )
+    ctx.lineTo( 47 , 27 )
+    ctx.fillStyle = '#6c757d';
+    ctx.strokeStyle = '#6c757d';
+    ctx.closePath()
+    ctx.fill()
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.moveTo( 28 , 42)
+    ctx.lineTo( 28 , 32)
+    ctx.lineTo( 23 , 32)
+    ctx.lineTo( 23 , 27 )
+    ctx.lineTo( 13 , 27 )
+    ctx.fillStyle = '#6c757d';
+    ctx.strokeStyle = '#6c757d';
+    ctx.closePath()
+    ctx.fill()
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.arc( 23 , 39 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = 'midnightblue';
+    ctx.fill()
+    ctx.closePath()
+
+    ctx.beginPath()
+    ctx.arc( 23 , 12 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = 'midnightblue';
+    ctx.fill()
+    ctx.closePath()
+
+    ctx.beginPath()
+    ctx.arc( 37 , 39 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = 'midnightblue';
+    ctx.fill()
+    ctx.closePath()
+
+    ctx.beginPath()
+    ctx.arc( 37 , 12 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = 'midnightblue';
+    ctx.fill()
+    ctx.closePath()
+
+
+    ctx.beginPath()
+    ctx.arc( 45 , 32 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = 'midnightblue';
+    ctx.fill()
+    ctx.closePath()
+
+    ctx.beginPath()
+    ctx.arc( 45 , 18 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = 'midnightblue';
+    ctx.fill()
+    ctx.closePath()
+
+    ctx.beginPath()
+    ctx.arc( 15 , 18 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = 'midnightblue';
+    ctx.fill()
+    ctx.closePath()
+
+    ctx.beginPath()
+    ctx.arc( 15 , 32 , 3 , Math.PI*0 , Math.PI*2 , true);
+    ctx.fillStyle = 'midnightblue';
+    ctx.fill()
+    ctx.closePath()
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
